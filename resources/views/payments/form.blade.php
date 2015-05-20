25a58de103cf03702bf9cc2241c92f368b73b90b
Kamaro Lambert <kamaroly@gmail.com>
2015-05-20 18:37:26 +0200

    Removed conflicts in the payment form

--- resources/views/payments/form.blade.php
+++ resources/views/payments/form.blade.php
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-@include('partials.css')
-@include('partials.js')
-<div class="ui segment">
-    <div class="ui fluid form">
-      <div class="two fields">
-        <div class="field">
-          <label>First Name</label>
-          <input placeholder="First Name" type="text">
-        </div>
-        <div class="field">
-          <label>Last Name</label>
-          <input placeholder="Last Name" type="text">
-        </div>
-      </div>
-      <div class="ui accordion field">
-        <div class="title">
-          <i class="icon dropdown"></i>
-          Optional Details
-        </div>
-        <div class="content field">
-          <label>Maiden Name</label>
-          <input placeholder="Maiden Name" type="text">
-        </div>
-      </div>
-      <div class="ui secondary submit button">Sign Up</div>
-    </div>
-  </div>
-  
-<script type="text/javascript">
-  $('.ui.accordion').accordion();
-</script>
-=======
 {!! Form::open(array('route' => 'payments.store','class'=>'ui form rahasi-form ' )) !!}
  <div class="inline  fields">
     <div class="required field" >
@@ -57,4 +24,3 @@
     </div>
 </div>
 {!! Form::close() !!}
->>>>>>> a9ecebf95308b7ef07514f5653835fe27cbe41e7
