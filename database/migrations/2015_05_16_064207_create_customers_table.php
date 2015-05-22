06242bf4a762bdd99a3a9d5a0b3a3c3c190a7d64
Kamaro Lambert <kamaroly@gmail.com>
2015-05-22 15:57:18 +0200

    Removed unnecessary fields in the customer table

--- database/migrations/2015_05_16_064207_create_customers_table.php
+++ database/migrations/2015_05_16_064207_create_customers_table.php
@@ -18,12 +18,6 @@ class CreateCustomersTable extends Migration {
 			$table->boolean('livemode')->default(false);
 			$table->string('description')->nullable();
 			$table->string('email')->nullable();
-			$table->string('delinquent'); //Whether or not the latest charge for the customer’s latest invoice has failed
-			$table->string('discount'); //Describes the current discount active on the customer, if there is.
-			$table->decimal('account_balance',10,2); //Current balance, if any, being stored on the customer’s account. If negative, the customer has credit to apply to the next invoice. If positive, the customer has an amount owed that will be added to the next invoice. 
-			$table->string('currency')->nullable();
-			$table->string('sources')->nullable();
-			$table->string('default_source')->nullable();
 			$table->integer('user_id')->unsigned();
 			$table->timestamps();
 
