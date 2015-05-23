468d449137084abf079dfed420cba833d87411c8
Kamaro Lambert <kamaroly@gmail.com>
2015-05-23 22:42:30 +0200

    Added some dumy dashboard

--- public/assets/js/dashboard.js
+++ public/assets/js/dashboard.js
@@ -1,20 +1,62 @@
+var data = [
+    {
+        value: 20,
+        color:"#637b85"
+    },
+    {
+        value : 30,
+        color : "#2c9c69"
+    },
+    {
+        value : 40,
+        color : "#dbba34"
+    },
+    {
+        value : 10,
+        color : "#c62f29"
+    }
+
+];
+var canvas = document.getElementById("hours");
+var ctx = canvas.getContext("2d");
+new Chart(ctx).Doughnut(data);
+
 
 var data = {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
-    datasets: [
-
+    labels : ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"],
+    datasets : [
         {
-            label: "Gross volume",
-            fillColor: "rgba(151,187,205,0.2)",
-            pointColor: "rgba(151,187,205,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(151,187,205,1)",
-            data: [28, 48, 40, 19, 86, 27, 90]
+            fillColor : "rgba(99,123,133,0.4)",
+            strokeColor : "rgba(220,220,220,1)",
+            pointColor : "rgba(220,220,220,1)",
+            pointStrokeColor : "#fff",
+            data : [65,54,30,81,56,55,40]
+        },
+        {
+            fillColor : "rgba(219,186,52,0.4)",
+            strokeColor : "rgba(220,220,220,1)",
+            pointColor : "rgba(220,220,220,1)",
+            pointStrokeColor : "#fff",
+            data : [20,60,42,58,31,21,50]
+        },
+    ]
+}
+var canvas = document.getElementById("shipments");
+var ctx = canvas.getContext("2d");
+new Chart(ctx).Line(data);
+
+var data = {
+    labels : ["Helpful","Friendly","Kind","Rude","Slow","Frustrating"],
+    datasets : [
+        {
+            fillColor : "rgba(220,220,220,0.5)",
+            strokeColor : "#637b85",
+            pointColor : "#dbba34",
+            pointStrokeColor : "#637b85",
+            data : [65,59,90,81,30,56]
         }
     ]
-};
-// Get the context of the canvas element we want to select
-var ctx = document.getElementById("dashboard").getContext("2d");
-
-var myLineChart = new Chart(ctx).Line(data);
+}
+var canvas = document.getElementById("departments");
+var ctx = canvas.getContext("2d");
+new Chart(ctx).Radar(data);
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,9 +3,40 @@
 @section('title') DASHBOARD @stop
 
 @section('content')
-
-
-<canvas id="dashboard"  class="ui grid sixteen columns" style="width: 100%;height: 30%;"></canvas>
+<div class="container clearfix">
+<div class="third widget doughnut">
+    <h3>Breakdown of Hours</h3>
+    <p><a href="" class="button">Filter By Employee</a></p>
+    <div class="canvas-container">
+        <canvas id="hours"></canvas>
+        <span class="status"></span>
+    </div>
+    <div class="chart-legend">
+        <ul>
+            <li class="ship">Shipping &amp; Receiving</li>
+            <li class="rework">Rework</li>
+            <li class="admin">Administrative</li>
+            <li class="prod">Production</li>
+        </ul>
+    </div>
+</div>
+    <div class="third widget line">
+        <div class="chart-legend">
+            <h3>Shipments per Day</h3>
+        </div>
+        <div class="canvas-container">
+            <canvas id="shipments"></canvas>
+        </div>
+    </div>
+    <div class="third widget">
+        <div class="chart-legend">
+            <h3>Customer Service Assessment</h3>
+        </div>
+        <div class="canvas-container">
+            <canvas id="departments"></canvas>
+        </div>
+    </div>
+</div>
 @stop
 
 @section('js')
