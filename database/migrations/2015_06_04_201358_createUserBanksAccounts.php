9c16d6e4d1ffd1931a4f5e3a08445031308109f3
Kamaro Lambert <kamaro@kamarombp.local>
2015-06-04 22:22:16 +0200

    Added user banks account

---
+++ database/migrations/2015_06_04_201358_createUserBanksAccounts.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateUserBanksAccounts extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create('user_banks', function (Blueprint $table) {
+			$table->increments('id');
+			$table->string('currency');
+			$table->string('bank_country');
+			$table->string('account_number');
+			$table->string('routing_number');
+
+			$table->integer('user_id')->unsigned();
+			$table->foreign('user_id')->references('id')->on('users');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::drop('user_banks');
+	}
+
+}
